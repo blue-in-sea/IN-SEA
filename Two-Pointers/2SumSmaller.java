06367bc214dd47335c926883ad89e9526fda7a5a
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-24 14:52:53 -0500

    Create 2SumSmaller.java

---
+++ Two-Pointers/2SumSmaller.java
@@ -0,0 +1,38 @@
+
+
+/*
+A[i] = {0  1  2  3  4  5  6}, target = 9, find the number of pairs that smaller than target
+Assume the input array is not null, and has length at leaste 1
+Assume duplicates exist 
+
+   0  1  2  3  4  5  6      
+   i ->           <- j
+
+本质: search in sorted matrix [每一行都是 sorted] [每一列都是 sorted]
+往下走：i++;
+往左走：j--;
+
+       0  1  2  3  4  5  6     [i]++
+    0 [0, 1, 2, 3, 4, 5, 6]     |
+    1 [1, 2, 3, 4, 5, 6, 7]
+    2 [2, 3, 4, 5, 6, 7, 8]
+    3 [3, 4, 5, 6, 7, 8, 9]  
+    4 [4, 5, 6, 7, 8, 9,10]   
+    5 [5, 6, 7, 8, 9,10,11]  
+
+                      <- [j]--
+                      
+step-1: starting from (0, 6) the top-right corner (where i = 0, j = lenArr - 1) 
+step-2: value = 6 < target = 9, 
+          res += 6 (j - i) then 
+          go down on row (i++)
+
+[note]: if the top-right value >= target
+          那么最后一列都不会有 target
+          可以直接 go right (j--)
+
+从右上角出发，拿 6 与 target 比较，可以知道是淘汰第一行，或者最后一行
+--> Time: O(n) where n is # row/col or length of array
+*/
+
+
