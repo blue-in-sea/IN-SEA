656bc6ae10cf33125b6dd955319ffffab62d2482
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-24 01:02:44 -0500

    Create CheckIfLinkedListHasACycle.java

---
+++ Two-Pointers/CheckIfLinkedListHasACycle.java
@@ -0,0 +1,28 @@
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class CheckIfLinkedListHasACycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
