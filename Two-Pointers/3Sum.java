acfccf3e3faf51a2bf7589bcdc8af052108076a6
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-24 00:50:52 -0500

    Create 3Sum.java

---
+++ Two-Pointers/3Sum.java
@@ -0,0 +1,31 @@
+class 3Sum {
+    // Time: O(nlogn) + O(n^2)
+    // Space: O(1)
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            // dedup all possiable duplicae i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    // dedup all possiable duplicae j
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+}
