cfd30005ee573a74dc4e2bba815461471d5fcb46
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-26 13:17:00 -0500

    Create 2SumTwoArrays.java

---
+++ Two-Pointers/2SumTwoArrays.java
@@ -0,0 +1,26 @@
+public class 2SumTwoArrays {
+    // 2 Sum: two pointers
+    // Time: O(m + n), Space: O(1)
+    public boolean existSum(int[] A, int[] B, int target) {
+        // Assume A and B are not null and have length at leaste 2
+        // Note A and B need to be both sorted!!
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int l = 0, r = B.length - 1;
+        int res = A[l] + B[r];
+
+        while (l < A.length && r >= 0) {
+            int sum = A[l] + B[r];
+            if (sum == target) {
+                return true;
+                }
+      if (sum < target) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+
+    return false;
+  }
+}
