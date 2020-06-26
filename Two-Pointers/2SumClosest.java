cadf984ccbe7da8232cc25bbd325ef511890de5d
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-06-26 02:41:15 -0500

    Update 2SumClosest.java

--- Two-Pointers/2SumClosest.java
+++ Two-Pointers/2SumClosest.java
@@ -1,32 +1,40 @@
+/**
+ * Find the pair of elements in a given array that sum to a value that is closest to the given target number. 
+ * Return the values of the two numbers.
+ */
+
 public class 2SumClosest {
-  // two pointers: search in sorted matrix
-  // time: O(nlogn) + O(n)
-  // space: O(1) in case of heap sort 
-  public List<Integer> closest(int[] array, int target) {
-    // Assume the given array is not not and has len of at leaste 2
-    Arrays.sort(array);
-
-    int l = 0, r = array.length - 1;
-    int res = array[l] + array[r];
-
-    List<Integer> pair = Arrays.asList(array[l], array[r]);
-    while (l < r) {
-      int sum = array[l] + array[r];
-      // update the result sum
-      if (Math.abs(target - sum) < Math.abs(target - res)) {
-        res = array[l] + array[r];
-        pair = Arrays.asList(array[l], array[r]);
-      }
-      // two pointers
-      if (sum > target) {
-        r--;
-      } else if (sum < target) {
-        l++;
-      } else {
-        return pair;
-      }
-    }
+    // two pointers: search in sorted matrix
+    // time: O(nlogn) + O(n)
+    // space: O(1) in case of heap sort 
     
-    return pair;
-  }
+    public List<Integer> closest(int[] array, int target) {
+        // Assume the given array is not not and has len of at leaste 2
+        Arrays.sort(array);
+        
+        int l = 0, r = array.length - 1;
+        int res = array[l] + array[r];
+ 
+        List<Integer> pair = Arrays.asList(array[l], array[r]);
+        while (l < r) {
+            int sum = array[l] + array[r];
+            
+            // update the result sum
+            if (Math.abs(target - sum) < Math.abs(target - res)) {
+                res = array[l] + array[r];
+                pair = Arrays.asList(array[l], array[r]);
+            }
+            
+            // two pointers
+            if (sum > target) {
+                r--;
+            } else if (sum < target) {
+                l++;
+            } else {
+                return pair;
+            }
+        }
+        
+        return pair;
+    }
 }
