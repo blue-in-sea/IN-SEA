b858d0d8051c93999c936d3be2f11e3a1bc4fbdb
IIIris <44347786+iris-in-sea@users.noreply.github.com>
2020-07-19 14:44:53 -0500

    Create PartitionEqualSubsetSum.java

---
+++ DP/PartitionEqualSubsetSum.java
@@ -0,0 +1,30 @@
+class PartitionEqualSubsetSum {
+    public boolean canPartition(int[] nums) {
+        int sum = 0; 
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+
+        Arrays.sort(nums);
+        return dfs(nums, 0, sum / 2);
+    }
+
+    private boolean dfs(int[] nums, int index, int target) {
+        if (target == 0) return true;
+        if (index == nums.length || target < 0) return false;
+
+        if (dfs(nums, index + 1, target - nums[index])) {
+            return true;
+        }
+        // 如果不加，就再也不加进来了
+        int j = index + 1;
+        while (j < nums.length && nums[index] == nums[j]) {
+            j++;
+        }
+        return dfs(nums, j, target);
+    }
+}
